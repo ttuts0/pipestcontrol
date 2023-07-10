fca3bfc6072271f914690ddc2a2787f8ea404c20
Tammy Mathews <tammymathews2006@gmail.com>
2023-07-10 15:08:41 -0700

    changed variable and function names

--- twosledmotion.py
+++ twosledmotion.py
@@ -1,19 +1,19 @@
 from gpiozero import MotionSensor, LED
 from signal import pause
 
-pir = MotionSensor(4)
-led1 = LED(16)
-led2 = LED(17)  
+pir = MotionSensor(4)#gpio4
+red = LED(16)#red
+white = LED(17)  #white
 
-def turn_on_leds():
-    led1.on()
-    led2.off()
+def turn_on_red():#turn on red
+    red.on()
+    white.off()
 
-def turn_off_leds():
-    led1.off()
-    led2.on()
+def turn_off_red():#TURN OFF
+    red.off()
+    white.on()
 
-pir.when_motion = turn_on_leds
-pir.when_no_motion = turn_off_leds
+pir.when_motion = turn_on_red
+pir.when_no_motion = turn_off_red
 
 pause()
