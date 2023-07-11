6e92f14c612e2ade9a81e648332c51e45c43de11
Tammy Mathews <tammymathews2006@gmail.com>
2023-07-11 11:52:16 -0700

    added third led

--- TimeDetection.py
+++ TimeDetection.py
@@ -1,19 +1,27 @@
 from gpiozero import MotionSensor, LED
 from datetime import datetime
 from signal import pause
+import random
 
 pir = MotionSensor(4)#gpio4
 red = LED(16)
 white = LED(17)
+green = LED(27)
 log = 'motionTimeLog.txt'
 
 def turn_on_red():#turn on red
-    red.on()
-    white.off()
+    val=random.randint(0,10)
+    print(val)
+    if val>5:
+        green.on()
+    else:
+        red.on()
     motion_log()
+    white.off()
 
 def turn_off_red():#TURN OFF
     red.off()
+    green.off()
     white.on()
     
 def motion_log():
