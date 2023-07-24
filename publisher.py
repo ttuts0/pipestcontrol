ccd40cb1dd696594ae929acbdac6e231c101f87f
Tammy Mathews <tammymathews2006@gmail.com>
2023-07-24 16:57:29 -0700

    publish

--- publisher.py
+++ publisher.py
@@ -1,17 +1,18 @@
 import paho.mqtt.client as mqtt
 import time
+#import TimeDetection
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-
+    message = "connected"
     # Send a message to the raspberry/topic every 1 second, 5 times in a row
-    for i in range(5):
-        # The four parameters are topic, sending content, QoS and whether retaining the message respectively
-        client.publish('raspberry/topic', payload=i, qos=0, retain=False)
-        print(f"send {i} to raspberry/topic")
+    # The four parameters are topic, sending content, QoS and whether retaining the message respectively
+    client.publish('pestbusterai/general', payload=message, qos=2, retain=False)
+    print(f"send '{message}' to raspberry/topic")
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.connect("broker.emqx.io", 1883, 60)
+def create_client():#parameter to change broker
+    client = mqtt.Client()
+    #client.on_connect = on_connect
+    client.connect("broker.emqx.io", 1883, 60)
 
-client.loop_forever()
+    return client
