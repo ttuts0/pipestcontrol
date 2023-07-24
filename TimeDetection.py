fdb40c176f90ca84f7363b9633d8a6bb4f017a0d
Tammy Mathews <tammymathews2006@gmail.com>
2023-07-24 16:56:28 -0700

    pest or friend

--- TimeDetection.py
+++ TimeDetection.py
@@ -4,6 +4,7 @@ from signal import pause
 import random
 import time
 import classify2
+import publisher
 
 pir = MotionSensor(4)#gpio4
 red = LED(16)
@@ -11,10 +12,14 @@ white = LED(17)
 green = LED(27)
 log = 'motionTimeLog.txt'              
 
+#create client function
+client = publisher.create_client()
+#client.publish('pestbusterai/motion', payload='hello', qos=0, retain=False)
+client.publish('pestbusterai/general', payload='connected', qos=0, retain=False)
 
 def motion_detected():#turn on red
     is_pest = classify2.search_for_pest()
-    print(is_pest)
+   
     if is_pest:
         red.on()  
         print("pest detected")
@@ -22,8 +27,10 @@ def motion_detected():#turn on red
         green.on()
         print("friend detected")
     motion_log(is_pest)
-    white.off()
+    detected_motion(is_pest)
     
+    white.off()
+    return is_pest
 
 # def pest_or_friend():
 #     return random.choice([True, False])
@@ -42,8 +49,13 @@ def motion_log(is_pest: bool):
         file.write('friend ')        
     file.write(f'detected at : {timestamp}\n')
     file.close()
-
-
+    
+def detected_motion(is_pest):
+    timestamp = datetime.now().strftime('%Y/%m/%d  %H:%M:%S')
+    if is_pest:
+        client.publish('pestbusterai/motion', payload='pest '+f'detected at : {timestamp}\n', qos=0, retain=False)
+    else:
+        client.publish('pestbusterai/motion', payload='friend '+f'detected at : {timestamp}\n', qos=0, retain=False)
 pir.when_motion = motion_detected 
 pir.when_no_motion = no_motion
 
