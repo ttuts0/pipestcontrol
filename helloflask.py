ac5a4f122496447a0256c753114518f836b4594d
Tammy Mathews <tammymathews2006@gmail.com>
2023-07-27 13:24:10 -0700

    shows last three instances of motion detected

--- helloflask.py
+++ helloflask.py
@@ -1,20 +1,45 @@
-from flask import Flask, request
+from flask import Flask, request, Response, stream_with_context, render_template
 from datetime import datetime
+import paho.mqtt.client as mqtt
+import time
 
-app = Flask(__name__)
+app = Flask(__name__)#creates a flask application instance and assinges it to variable app, name is to figure out the path 
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    # Subscribe, which need to put into on_connect
+    client.subscribe("pestbusterai/motion")
+
+def print_msg(client, userdata, msg):#function will be called when the MQTT client (client) receives a message on the subscribed topic.
+    f=open("motion_log.txt", "a")
+    f.write(f"{msg.payload}\n")
+    f.close()
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = print_msg
+client.connect("broker.emqx.io", 1883, 60)
+
+def get_last_three_lines(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    return lines[-3:]
+
 
 @app.route('/')
-def hello():
-    name = request.args['name']
-    return HELLO_HTML.format(
-            name, str(datetime.now()))
+def stream_file():
+    file_path = 'motion_log.txt'
+    last_three_lines = get_last_three_lines(file_path)
+    
+    return render_template('index.html', lines=last_three_lines)
 
-HELLO_HTML = """
-    <html><body>
-        <h1>Hello, {0}!</h1>
-        The time is {1}.
-    </body></html>"""
+#HELLO_HTML = """
+ #   <html><body>
+  #      <h1>{0}</h1>
+   #     The time is {1}.
+    #</body></html>"""
 
 if __name__ == "__main__":
     # Launch the Flask dev server
+    client.loop_start()
     app.run(host="localhost", debug=True)
--- subscriber.py
+++ subscriber.py
@@ -5,21 +5,17 @@ def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     # Subscribe, which need to put into on_connect
     # If reconnect after losing the connection with the broker, it will continue to subscribe to the raspberry/topic topic
-    client.subscribe("pestbusterai/motion")
 
 # The callback function, it will be triggered when receiving messages
 def on_message(client, userdata, msg):
-    print(f"{msg.topic} {msg.payload}")
+    print(f"{msg.payload}")
 
-#def create_clienttwo():
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+def create_client():
+    client.on_message = on_message
 
-# Set the will message, when the Raspberry Pi is powered off, or the network is interrupted abnormally, it will send the will message to other clients
-#clienttwo.will_set('raspberry/status', b'{"status": "Off"}')
+    # Set the will message, when the Raspberry Pi is powered off, or the network is interrupted abnormally, it will send the will message to other clients
+    #clienttwo.will_set('raspberry/status', b'{"status": "Off"}')
 
-# Create connection, the three parameters are broker address, broker port number, and keep-alive time respectively
-client.connect("broker.emqx.io", 1883, 60)
+    # Create connection, the three parameters are broker address, broker port number, and keep-alive time respectively
 
-client.loop_forever()
+    return client 
