f8d0403b7c5a69c241830fce62e8b1aa2c0b2b6d
Tammy Mathews <tammymathews2006@gmail.com>
2023-08-02 16:02:54 -0700

    stats table working

--- helloflask.py
+++ helloflask.py
@@ -2,38 +2,52 @@ from flask import Flask, request, Response, stream_with_context, render_template
 from datetime import datetime
 import paho.mqtt.client as mqtt
 import time
+from collections import defaultdict  # Import defaultdict
 
 app = Flask(__name__)#creates a flask application instance and assinges it to variable app, name is to figure out the path 
 
-def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
-    # Subscribe, which need to put into on_connect
-    client.subscribe("pestbusterai/motion")
+def get_motion_data_from_file(file_path):
+    #motion_data =   defaultdict(lambda: {"pest": 0, "friend": 0})
+    motion_data={}
+    for i in range(24):
+        motion_data[i] = {"pest": 0, "friend": 0}
+    with open(file_path, 'r') as file:
+        for line in file: 
+            parts = line.strip().split(',')
+            if len(parts) == 2:
+                print(parts[0])
+                event = parts[0].strip()
+                timestamp = parts[1].strip()
+                hour = int(timestamp.split(' ')[-1].split(':')[0])
+                if event == 'pest':
+                    motion_data[hour]['pest'] += 1
+                elif event == 'friend':
+                    motion_data[hour]['friend'] += 1
 
-def print_msg(client, userdata, msg):#function will be called when the MQTT client (client) receives a message on the subscribed topic.
-    f=open("motion_log.txt", "a")
-    f.write(f"{msg.payload}\n")
-    f.close()
+    return motion_data
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = print_msg
-client.connect("broker.emqx.io", 1883, 60)
 
 def get_last_three_lines(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
-    return lines[-3:]
+    formatted_lines = []
+    for line in lines[-3:]:
+        formatted_lines.append(str(line))
+    return formatted_lines
 
 def get_rest_of_lines(file_path):
     with open(file_path, 'r') as file:
-        lines = file.readlines()
-    return lines[:-3]
+        lines= file.readlines()
+    formatted_lines = []
+    for line in lines[:-3]:
+        formatted_lines.append(str(line))
+    return formatted_lines
 
 @app.route('/')
 def stream_file():
     file_path = 'motion_log.txt'
     last_three_lines = get_last_three_lines(file_path)
+    #motion_data = get_motion_data_from_file(file_path)
     #formatted_lines = format_lines(last_three_lines)
     return render_template('index.html', lines=last_three_lines)
 
@@ -43,6 +57,13 @@ def show_full_log():
     rest_of_lines = get_rest_of_lines(file_path)
     #formatted_lines = format_lines(rest_of_lines)
     return render_template('show_more.html', lines=rest_of_lines)
+
+@app.route('/stats')
+def stats():
+    file_path = 'motion_log.txt'
+    motion_data = get_motion_data_from_file(file_path)
+    motion_data_copy = dict(motion_data) 
+    return render_template('stats.html', motion_data=motion_data)
 #HELLO_HTML = """
  #   <html><body>
   #      <h1>{0}</h1>
@@ -51,5 +72,4 @@ def show_full_log():
 
 if __name__ == "__main__":
     # Launch the Flask dev server
-    client.loop_start()
     app.run(host="localhost", debug=True)
