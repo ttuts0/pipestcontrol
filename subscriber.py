dac44819ac079b9d77588f8d85213748feec5c5a
Tammy Mathews <tammymathews2006@gmail.com>
2023-08-04 13:27:32 -0700

    publishing img effectively

--- subscriber.py
+++ subscriber.py
@@ -1,11 +1,12 @@
 # subscriber.py
 import paho.mqtt.client as mqtt
-
+import base64 
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     # Subscribe, which need to put into on_connect
     client.subscribe("pestbusterai/motion")
+    client.subscribe("pestbusterai/image")
 
 def print_msg(client, userdata, msg):#function will be called when the MQTT client (client) receives a message on the subscribed topic.
     f=open("motion_log.txt", "a")
@@ -13,8 +14,17 @@ def print_msg(client, userdata, msg):#function will be called when the MQTT clie
     f.write(message+'\n')
     f.close()
 
+def print_msg2(client, userdata, msg):
+    message2 = str(msg.payload.decode('utf-8'))
+    img =message2.encode('ascii')
+    final_msg=base64.b64decode(img)
+    open('recive_img.jpg','wb').write(final_msg)
+
 client = mqtt.Client()
 client.on_connect = on_connect
-client.on_message = print_msg
+#client.on_message = print_msg
+#client.on_message=print_msg2
+client.message_callback_add("pestbusterai/motion", print_msg)
+client.message_callback_add("pestbusterai/image", print_msg2)
 client.connect("broker.emqx.io", 1883, 60)
 client.loop_forever()
